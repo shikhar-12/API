9d31b2c1487d758a64590b60f646bda2a48ff9d7
shikhar-12 <shikhars74@gmail.com>
2024-02-17 12:38:59 +0530

    stripe

--- Routes/Web..js
+++ Routes/Web..js
@@ -4,6 +4,7 @@ const UC = require("../Controller/UserController");
 const PC = require("../Controller/profileController");
 const CC = require("../Controller/CategoryController");
 const PRC = require("../Controller/ProductController");
+const PaymentController = require("../Controller/PaymentController");
 const checkuserauth = require("../Middleware/auth");
 
 router.get("/getalluser", checkuserauth, UC.getalluser);
@@ -26,5 +27,12 @@ router.get("/getallproduct", PRC.GetAllProduct);
 router.get("/getproductdetail/:id", PRC.GetProductDetail);
 router.post("/updateproduct/:id", PRC.UpdateProduct);
 router.post("/deleteproduct/:id", PRC.DeleteProduct);
+
+router.post(
+  "/payment/process",
+  checkuserauth,
+  PaymentController.processPayment
+);
+router.get("/stripeapikey", checkuserauth, PaymentController.sendStripeApiKey);
 
 module.exports = router;
